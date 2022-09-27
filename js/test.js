38ca20664a8c22395755b60ec772fb977c509ab2
rahmangngor <rahman.gungor@invivosoftware.com>
2022-09-27 22:29:26 +0300

    devexpress exmple --- will delete

--- js/test.js
+++ js/test.js
@@ -25,3 +25,39 @@ function drawChart() {
     var chart = new google.visualization.LineChart(document.getElementById('myChart'));
     chart.draw(data, options);
 }
+
+//---------------------------------------
+//Devexpress AutoComplete
+$(function () {
+    const dataSource = new DevExpress.data.DataSource({
+        store: {
+            type: 'array',
+            data: employeesTasks,
+            key: 'ID',
+        },
+        //group: 'Assignee',
+    });
+
+    $('#autocomplete').dxAutocomplete({
+        dataSource,
+        valueExpr: "ID",
+        searchExpr: ["Assignee", "ID", "Subject", "tel"],
+        label: "Search for a task",
+        labelMode: "floating",
+        //grouped: true,
+        showClearButton: true,
+        // groupTemplate ({ key }) {
+        //     return "Assigned to " + key;
+        // },
+        onValueChanged: function (e) {
+            console.log(e.value);
+            console.log(e.previousValue);
+        },
+        itemTemplate({ Subject, Assignee }, index) {
+            return index + 1 + '. ' + Subject + '-' + Assignee;
+        },
+        wrapItemText: true,
+        searchTimeout: 300,
+        maxLength: 20,
+    });
+});
